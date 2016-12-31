d8b86f9521f999119c7def5d7f632eca73b754ce
coristig <colin.ristig@gmail.com>
2016-12-30 19:11:09 -0800

    added metadata

--- trainmodel-demo/train.py
+++ trainmodel-demo/train.py
@@ -1,7 +1,12 @@
+from bandit import *
 import pandas as pd
 import statsmodels.formula.api as sm
+
 df = pd.DataFrame({"A": [10,20,30,40,50], "B": [20, 30, 10, 40, 50], "C": [32, 234, 23, 23, 42523]})
 result = sm.ols(formula="A ~ B + C", data=df).fit()
-print(result.params)
+metadata = {'R2': result.rsquared, 'AIC': result.aic}
+
+bandit.Metadata(metadata)
+print(metadata)
 
 df.to_csv('/job/output-files/dataframe.csv')
