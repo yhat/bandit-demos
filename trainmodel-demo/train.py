b7d9a18d1b1b6191dcf89d651f5e5620af17d3a9
coristig <colin.ristig@gmail.com>
2016-12-31 13:00:14 -0800

    reqs

--- trainmodel-demo/train.py
+++ trainmodel-demo/train.py
@@ -4,9 +4,10 @@ import statsmodels.formula.api as sm
 
 df = pd.DataFrame({"A": [10,20,30,40,50], "B": [20, 30, 10, 40, 50], "C": [32, 234, 23, 23, 42523]})
 result = sm.ols(formula="A ~ B + C", data=df).fit()
-metadata = {'R2': result.rsquared, 'AIC': result.aic}
 
+metadata = {'R2': result.rsquared, 'AIC': result.aic}
 bandit.Metadata(metadata)
+
 from time import gmtime, strftime
 print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 print(metadata)
