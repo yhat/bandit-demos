bafb3a8a2004e1307b0f6e72f9f7079c2cf1ca5a
coristig <colin.ristig@gmail.com>
2016-12-31 16:41:01 -0800

    moved reqs

--- trainmodel-demo/train.py
+++ trainmodel-demo/train.py
@@ -6,6 +6,8 @@ df = pd.DataFrame({"A": [10,20,30,40,50], "B": [20, 30, 10, 40, 50], "C": [32, 2
 result = sm.ols(formula="A ~ B + C", data=df).fit()
 
 metadata = {'R2': result.rsquared, 'AIC': result.aic}
+
+bandit = Bandit('colin', 'c4548110-cc4b-11e6-a5c5-0242ac110003','http://54.201.192.120/')
 bandit.Metadata(metadata)
 
 from time import gmtime, strftime
