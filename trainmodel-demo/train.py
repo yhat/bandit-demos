1eb3f6a86cae5ea54b92dcd51f7816d254462bf2
coristig <colin.ristig@gmail.com>
2017-01-03 10:16:17 -0800

    report

--- trainmodel-demo/train.py
+++ trainmodel-demo/train.py
@@ -1,6 +1,6 @@
 from bandit import Bandit
 import pandas as pd
-import random
+import numpy as np
 import statsmodels.formula.api as sm
 
 df = pd.DataFrame({"A": [10,20,30,40,50], "B": [20, 30, 10, 40, 50], "C": [32, 234, 23, 23, 42523]})
