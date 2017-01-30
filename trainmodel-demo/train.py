6187ce344456b2a008d18dd493dc3844e1b31ef3
coristig <colin.ristig@gmail.com>
2017-01-30 12:34:20 -0500

    updated paths

--- trainmodel-demo/train.py
+++ trainmodel-demo/train.py
@@ -8,6 +8,7 @@ import matplotlib
 matplotlib.use('Agg')
 import seaborn as sns
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 df = pd.DataFrame({ \
     "A": np.random.normal(100,10,50).tolist(), \
@@ -18,7 +19,7 @@ result = sm.ols(formula="A ~ B + C", data=df).fit()
 
 metadata = {'R2': result.rsquared, 'AIC': result.aic}
 
-with open("model_stats.txt", "w") as text_file:
+with open(output_dir + 'model_stats.txt', "w") as text_file:
     model_summary = str(result.summary())
     text_file.write(model_summary)
 
@@ -35,10 +36,10 @@ bandit.metadata.R2 = result.rsquared
 bandit.metadata.AIC = result.aic
 
 chart = sns.distplot(df.A)
-chart.figure.savefig('/job/output-files/dist.png')
+chart.figure.savefig(output_dir + 'dist.png')
 
 # save
-df.head().to_csv('/job/output-files/datasample.csv')
+df.head().to_csv(output_dir + 'datasample.csv')
 
 today = datetime.date.today().strftime('%Y_%m_%d')
 
@@ -54,7 +55,7 @@ Model Stats: %s
 ''' % result.model.formula, result.rsquared_adj
 
 email.body = body
-email.add_attachment('/job/output-files/datasample.csv')
-email.add_attachment('/job/output-files/model_stats.txt')
-email.add_attachment('/job/output-files/dist.png')
+email.add_attachment(output_dir + 'datasample.csv')
+email.add_attachment(output_dir + 'model_stats.txt')
+email.add_attachment(output_dir + 'dist.png')
 email.send('colin@yhathq.com')
